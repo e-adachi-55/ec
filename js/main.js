6bab83c5267904687ee06ec2a3a97ea5dc8beb48
e-adachi-55 <64426498+e-adachi-55@users.noreply.github.com>
2020-06-28 23:20:36 +0900

    twelve commit
    アコーディオン・レビュー選択機能実装

--- js/main.js
+++ js/main.js
@@ -37,6 +37,32 @@ $(function() {
 		$(this).siblings('li').removeClass('is-active');
 		$('.item-size-select span').text(select_size);
 	});
+
+	//レビュー
+	let review_num = 0;
+	$('.review li').on('click',function() {
+		if(review_num === $('.review li').index(this) + 1) {
+			$('.review li').removeClass('is-active');
+			review_num = 0;
+		} else {
+			review_num = $('.review li').index(this) + 1;
+			$('.review li').removeClass('is-active');
+			$(`.review li:lt('${review_num}')`).addClass('is-active');
+		}
+	});
+
+	//アイテム詳細説明文アコーディオン
+	$('[data-accordion="trigger"]').on('click', function() {
+		$(this).toggleClass('is-active');
+		$(this).next().slideToggle();
+	});
+
+	//スマホ時のサイドバー開閉
+	$('.sidebar-title').on('click',function(){
+		$(this).toggleClass('is-active');
+		$(this).next('.sidebar-body').stop().toggle();
+	});
+
 });
 
 $(window).on("scroll", function() {
